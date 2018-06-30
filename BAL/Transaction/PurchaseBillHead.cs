0b92f1c0aed66b67a8b62da805def929708672f9
MaheshNemane <40641811+MaheshNemane@users.noreply.github.com>
2018-06-30 15:31:46 +0530

    class

--- BAL/Transaction/PurchaseBillHead.cs
+++ BAL/Transaction/PurchaseBillHead.cs
@@ -8,5 +8,20 @@ namespace BAL.Transaction
 {
     public class PurchaseBillHead
     {
+        public Int32 PurchaseId { get; set; }
+        public Int32 InvoiceNo { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public Int32 SupplierId { get; set; }
+        public Int32 PaymentId { get; set; }
+        public decimal TotalCGST { get; set; }
+        public decimal TotalSGST { get; set; }
+        public decimal TotalIGST { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Other { get; set; }
+        public decimal GrandTotal { get; set; }
+        public string Status { get; set; }
+        public string cmd { get; set; }
+        List<PurchaseBillDetails> PurchaseList { get; set; }
+
     }
 }
