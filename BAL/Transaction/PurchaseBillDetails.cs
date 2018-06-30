1f1dfada28d805ee242a79cb72cea5e8a54e8333
MaheshNemane <40641811+MaheshNemane@users.noreply.github.com>
2018-06-30 15:31:20 +0530

    class

--- BAL/Transaction/PurchaseBillDetails.cs
+++ BAL/Transaction/PurchaseBillDetails.cs
@@ -8,5 +8,20 @@ namespace BAL.Transaction
 {
     public class PurchaseBillDetails
     {
+
+        public Int32 Id { get; set; }
+        public Int32 PurchaseId { get; set; }
+        public Int32 CategoryId { get; set; }
+        public Int32 SubCategoryId { get; set; }
+        public Int32 ItemId { get; set; }
+        public Double Qty{ get; set; }
+        public Double Price { get; set; }
+        public String Unit { get; set; }
+        public decimal TotalCGST { get; set; }
+        public decimal TotalSGST { get; set; }
+        public decimal TotalIGST { get; set; }
+        public decimal Total { get; set; }
+        public string Status { get; set; }
+        
     }
 }
